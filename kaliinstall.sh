b8c24e6b25908ef89e423fe1e726bbc9a21f0faf
Etienne Ducharme <etienne.ducharme@gmail.com>
2015-03-18 15:44:15 -0400

    Replace dash with underscore

--- kaliinstall.sh
+++ kaliinstall.sh
@@ -2,7 +2,7 @@ cp lib/msf/core/auxiliary/* /usr/share/metasploit-framework/lib/msf/core/auxilia
 echo "require 'msf/core/auxiliary/sip'" >> /usr/share/metasploit-framework/lib/msf/core/auxiliary/mixins.rb
 echo "require 'msf/core/auxiliary/skinny'" >> /usr/share/metasploit-framework/lib/msf/core/auxiliary/mixins.rb
 cp modules/auxiliary/voip/viproy* /usr/share/metasploit-framework/modules/auxiliary/voip/
-cp modules/auxiliary/spoof/cisco/viproy-cdp.rb /usr/share/metasploit-framework/modules/auxiliary/spoof/cisco/
+cp modules/auxiliary/spoof/cisco/viproy_cdp.rb /usr/share/metasploit-framework/modules/auxiliary/spoof/cisco/
 printf "You can execute msfconsole now.\nViproy modules placed under auxiliary/voip/viproy*\n"
 apt-get install libpcap-dev
 gem install pcaprub
